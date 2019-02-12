4fa89ae676699bf2c7d970834e92bb0108f8766b
emmastockton <emmastockton10@gmail.com>
2019-02-12 21:50:58 +0000

    PUT method now created

--- tasks.js
+++ tasks.js
@@ -67,50 +67,7 @@ app.delete('/tasks/:taskId', function (request, response) {
 
 app.put('/tasks/:taskId', function (request, response) {
 
-  let tasks = [
-    {
-      description: "Climb Mt Kilimanjaro",
-      id: 1,
-      completed: false
-    },
-    {
-      description: "Do the Inca Trail",
-      id: 2,
-      completed: false
-    },
-    {
-      description: "Go skydiving",
-      id: 3,
-      completed: false
-    },
-    {
-      description: "Learn how to play an instrument",
-      id: 4,
-      completed: false
-    },
-    {
-      description: "Get a dog",
-      id: 5,
-      completed: false
-    }
-  ];
-
-  const taskIdToComplete = request.params.taskId;
-
-  const filteredTask = tasks.filter(task => task.id == taskIdToComplete);
-
-  let taskToComplete = filteredTask[0];
-
-  taskToComplete.completed = true;
-
-  let taskPosition = tasks.indexOf(taskToComplete);
-
-    for (let x = 0; x < 1; x ++) {
-        tasks.splice(taskPosition, 1);
-        tasks.push(taskToComplete);
-    };
-
-  response.json(tasks);
+  response.json({message: "You have tried to change task " + request.params.taskId});
 
 });
 
